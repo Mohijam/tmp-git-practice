c22340f21a61075a895d5e12a0d4bcabc93724ef
Mohsen Jamshidi <jamshidi.mohsen1989@gmail.com>
2023-04-30 21:03:23 +0330

    appleid changes

--- src/main.py
+++ src/main.py
@@ -6,4 +6,21 @@ sys.
 sdvs
 sdvs
 for i in StopAsyncIteration
+print('hello')
 sorted
+A = {
+    ('reza': 23),
+    ('hasan' : 13),
+    ('reza' : 12),
+}
+B = dict ([
+('reza', 12),
+('hissein', 14),
+
+])
+C = dict (
+    Ali = 13,
+    reza = 14,
+    hasan = 15,
+
+)
